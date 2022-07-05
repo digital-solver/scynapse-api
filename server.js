95290466abf00d3f677974277be250ed3c5f190e
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-07-05 18:10:48 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// Imports
+const http = require('http'),
+    url = require('url'),
+    fs = require('fs');
+
+// Server Setup & URL Management
+http.createServer((request, response) => {
+    let addr = request.url,
+        q = url.parse(addr, true),
+        filePath = '';
+
+    // Update Log
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+    // Parse and POST URL
+    if (q.pathname.includes('documentation')) {
+        filePath = __dirname + '/documentation.html';
+    } else {
+        filePath = __dirname + '/index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text / plain' });
+        response.write(data);
+        response.end();
+    });
+}).listen(8080);
+console.log('Server: Up\nPort: 8080');
