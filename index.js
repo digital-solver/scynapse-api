9d499e76889351261c1c35ced9f079a3b4d21e28
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-07-20 15:28:36 +0200

    Updates for achievement 2.5

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ const topTenMovies = [
   },
 ];
 
+const users = [
+  {
+    name: 'Jane Smith',
+    username: 'jsmith98',
+    password: 'secret123',
+    id: '1',
+  },
+];
+
 // MIDDLEWARE
 
 const requestTime = (req, res, next) => {
@@ -88,6 +97,38 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
   res.json(topTenMovies);
+});
+
+app.get('/movies/:title', (req, res) => {
+  res.send('A JSON Object holding data avout a single movie contining a title, description, genre, actors, composer, ost, and director.');
+});
+
+app.get('/movies/:genre', (req, res) => {
+  res.send('A text message containing a description about a genre.');
+});
+
+app.get('/movies/:director', (req, res) => {
+  res.send('A JSON object containing data about a director.');
+});
+
+app.post('/users', (req, res) => {
+  res.send('A JSON Object Holding data about the user added.');
+});
+
+app.put('/users/:username', (req, res) => {
+  res.send('A text message indicating whether the info was successfully updated.');
+});
+
+app.delete('/users/:username', (req, res) => {
+  res.send('A text message indicating whether the user was successfully deregistered.');
+});
+
+app.post('/users/:username/favourites/:title', (req, res) => {
+  res.send('A text message indicating whether the movie was successfully added to favourites.');
+});
+
+app.delete('/users/:username/favourites/:title', (req, res) => {
+  res.send('A text message indicating whether the movie was successfully removed from favourites.');
 });
 
 app.listen(8080, () => {
