becbdbd59a695779597f3c6e603a3621fd4a1b3d
Kerr Campbell - The Digital Solver <kerr.digitalsolver@gmail.com>
2023-01-01 17:23:44 +0000

    Updating documentation

--- passport.js
+++ passport.js
@@ -9,6 +9,13 @@ const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
 // PASSPORT STRATEGIES
+
+/**
+ * A local passport strategy for authenticating users.
+ * @param {string} username - The username of the user.
+ * @param {string} password - The password of the user.
+ * @param {function} callback - The callback function to be called upon completion of the strategy.
+ */
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password',
@@ -32,9 +39,15 @@ passport.use(new LocalStrategy({
   });
 }));
 
+/**
+ * A JWT passport strategy for authenticating users.
+ * @param {string} jwtFromRequest - The JWT from the request.
+ * @param {string} secretKey - The secret key for validating the JWT.
+ * @param {function} callback - The callback function to be called upon completion of the strategy.
+ */
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret',
+  secretKey: 'your_jwt_secret',
 }, (jwtPayload, callback) => Users.findById(jwtPayload._id)
   .then((user) => callback(null, user))
   .catch((error) => callback(error))));
